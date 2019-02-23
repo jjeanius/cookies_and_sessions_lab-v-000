30d93a1da6c7085d50f3a262b7e449accd180e4b
Judy McGoldrick <jjeanius@verizon.net>
2019-02-23 03:55:00 +0000

    Automatically backed up by Learn

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def homepage
+    @product= Product.find(paramd[:id])
+      cart << @product.id
+      session[:cart] = cart
+  end
+
+end
